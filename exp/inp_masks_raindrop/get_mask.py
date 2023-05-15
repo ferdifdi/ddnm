7f2c4e3281dd165d7dfed6cd5cb3a788c97617f4
ferdifdi <ferdifadillah3@gmail.com>
2023-05-15 14:33:00 +0000

    update

--- exp/inp_masks_raindrop/get_mask.py
+++ exp/inp_masks_raindrop/get_mask.py
@@ -1,11 +1,23 @@
 import numpy as np
 import cv2
+import argparse
 
-mask = cv2.imread("./orig_5.png")
-print(mask.shape)
-mask = mask[:, :, 0]
-mask = (mask == 255) * 1
-print(mask.shape)
-np.save("mask.npy", mask)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+#     print(args.file)
+    mask = cv2.imread("/workspace/DDNM/exp/inp_masks_raindrop/mask_th05_all/"+args.file)
+#     print(mask)
+    print(mask.shape)
+    mask = mask[:, :, 0]
+    mask = (mask == 255) * 1
+    print(mask.shape)
+    np.save("/workspace/DDNM/exp/inp_masks_raindrop/mask.npy", mask)
 
 
